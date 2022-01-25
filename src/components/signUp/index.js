ffb3463a637d60c7136d2764c5fa85364c8f3cae
Claudio <claudio.junior233@gmail.com>
2022-01-25 15:32:43 -0300

    .

--- src/components/signUp/index.js
+++ src/components/signUp/index.js
@@ -1,11 +1,24 @@
 import { UnitContext } from "../../context/unitContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import axios from "axios";
 export default function SignUpUnit() {
   const { setMode } = useContext(UnitContext);
+  const [nickname, setNickname] = useState();
+  const [locale, setLocale] = useState();
+  const [brand, setBrand] = useState();
+  const [model, setModel] = useState();
+
   function handleSubmit(e) {
     e.preventDefault();
     setMode("unitSignUp");
+    axios.post("http://localhost:3333/unidades", {
+      apelido: nickname,
+      local: locale,
+      marca: brand,
+      modelo: model,
+    });
   }
+
   return (
     <>
       <div>
@@ -15,6 +28,46 @@ export default function SignUpUnit() {
           }}
         >
           <p>CADASTRO</p>
+          <label>
+            Apelido
+            <input
+              type="text"
+              onChange={(e) => {
+                setNickname(e.target.value);
+              }}
+            ></input>
+          </label>
+          <label>
+            Local
+            <input
+              type="text"
+              onChange={(e) => {
+                setLocale(e.target.value);
+              }}
+            ></input>
+          </label>
+          <label>
+            Marca
+            <input
+              type="text"
+              onChange={(e) => {
+                setBrand(e.target.value);
+              }}
+            ></input>
+          </label>
+          <label>
+            Modelo
+            <input
+              type="text"
+              onChange={(e) => {
+                setModel(e.target.value);
+              }}
+            ></input>
+          </label>
+          <label>
+            Ativo
+            <input type="checkbox"></input>
+          </label>
           <input type="submit" value="Cadastrar"></input>
         </form>
       </div>
