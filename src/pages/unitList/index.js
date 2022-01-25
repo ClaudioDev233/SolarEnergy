cfcfc6b4987ebe57e53f376ca105a2f7ea5672e1
Claudio <claudio.junior233@gmail.com>
2022-01-25 12:04:06 -0300

    added context

--- src/pages/unitList/index.js
+++ src/pages/unitList/index.js
@@ -1,10 +1,42 @@
 import NavMenu from "../../components/menu";
-
+import { useState } from "react";
+// mudar para units ou algo do genero e apagar o unit sing up
 export default function UnitList() {
+  const [mode, setMode] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(mode);
+    setMode("unitSignUp");
+  }
   return (
     <>
-      <p>UNIDADE DE EERGIA</p>
-      <NavMenu></NavMenu>
+      {/* <NavMenu></NavMenu> */}
+      <h1>Unidades</h1>
+      {mode === "units" ? (
+        <div>
+          <form
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}
+          >
+            <p>CADASTRO</p>
+            <input type="submit" value="Cadastrar"></input>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <p>Unidaditas</p>
+          <button
+            onClick={() => {
+              setMode("units");
+              console.log(mode);
+            }}
+          >
+            Nova Unidade
+          </button>
+        </div>
+      )}
     </>
   );
 }
